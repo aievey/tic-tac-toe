8959f1e9d59bd64a1b6c22d63c5cea3fce82e698
Amitabh Verma <amitabh283verma@gmail.com>
2020-04-07 20:24:58 +0530

    Updates

---
+++ precache-manifest.ea28dfd046364910626465f571a1f500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b631c6fe3d4cbc88fb25b80dcc6fe369",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "bd5227eb85c885463457",
+    "url": "/tic-tac-toe/static/css/main.f6b7a22c.chunk.css"
+  },
+  {
+    "revision": "df3e18ec0b0d0ce674cc",
+    "url": "/tic-tac-toe/static/js/2.b87816fb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tic-tac-toe/static/js/2.b87816fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd5227eb85c885463457",
+    "url": "/tic-tac-toe/static/js/main.7f9c46ab.chunk.js"
+  },
+  {
+    "revision": "40929888a404a7396a7f",
+    "url": "/tic-tac-toe/static/js/runtime-main.061e70df.js"
+  }
+]);
